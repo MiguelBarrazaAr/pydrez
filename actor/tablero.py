cb476802bfbdb7eb51b019d68d15bf1f51f2d254
barrazamiguel <correo@miguelbarraza.com.ar>
2017-08-31 08:11:54 -0300

    agregado los peones al tablero

--- actor/tablero.py
+++ actor/tablero.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from pilasengine.actores.actor import Actor
 
 from celda import *
@@ -6,6 +7,17 @@ class Tablero(Actor):
     """Representa al tablero"""
 
     def __init__(self, pilas, x=0, y=0, alto=8, ancho=8):
+        """Constructor del tablero:
+        :param x: x del punto central de a1 (casilla inferior izquierda).
+        :type x: int
+        :param y: y del punto central de a1
+        :type y: int
+        :param ancho: cantidad de casillas a lo ancho del tablero
+        :type ancho: int
+        :param alto: cantidad de casillas a lo alto.
+        :type ancho: int
+        """
+
         self._alto = alto
         self._ancho = ancho
         self._x = x
@@ -39,6 +51,16 @@ class Tablero(Actor):
                 negro = not negro
 
     def posicionar(self, actor, x, y):
-        """Posiciona un actor en una casilla del tablero"""
-        actor.x=self._x+(x-1)*self.distancia
-        actor.y=self._y+(y-1)*self.distancia
+        """Posiciona un actor en una casilla del tablero
+        y lo almacena como actor activo dentro del tablero.
+
+        :param actor: un actor a posicionar en una casilla del tablero.
+        :type actor: Actor
+        :param x: numero de columna en la que se posicionará (0..n)
+        :type x: int
+        :param y: numero de fila (0..n)
+        :type y: int
+        """
+
+        actor.x=self._x+x*self.distancia
+        actor.y=self._y+y*self.distancia
--- logica/ajedrez_basico.py
+++ logica/ajedrez_basico.py
@@ -1,20 +1,29 @@
+# -*- encoding: utf-8 -*-
+
 from actor.alfil import Alfil
 from actor.caballo import Caballo
+from actor.peon import Peon
 from actor.reina import Reina
 from actor.rey import Rey
 from actor.torre import Torre
 
 
 def armarAjedrezBasico(pilas, tablero):
-    acomodarPiezas(pilas, tablero, fila=1, color="blanco")
-    acomodarPiezas(pilas, tablero, fila=8, color="negro")
+    acomodarPiezas(pilas, tablero, fila=0, color="blanco")
+    acomodarPeones(pilas, tablero, fila=1, color="blanco")
+    acomodarPiezas(pilas, tablero, fila=7, color="negro")
+    acomodarPeones(pilas, tablero, fila=6, color="negro")
 
 def acomodarPiezas(pilas, tablero, fila, color):
-    tablero.posicionar(Torre(pilas, color=color), 1, fila)
-    tablero.posicionar(Caballo(pilas, color=color), 2, fila)
-    tablero.posicionar(Alfil(pilas, color=color), 3, fila)
-    tablero.posicionar(Reina(pilas, color=color), 4, fila)
-    tablero.posicionar(Rey(pilas, color=color), 5, fila)
-    tablero.posicionar(Alfil(pilas, color=color), 6, fila)
-    tablero.posicionar(Caballo(pilas, color=color), 7, fila)
-    tablero.posicionar(Torre(pilas, color=color), 8, fila)
+    tablero.posicionar(Torre(pilas, color=color), 0, fila)
+    tablero.posicionar(Caballo(pilas, color=color), 1, fila)
+    tablero.posicionar(Alfil(pilas, color=color), 2, fila)
+    tablero.posicionar(Reina(pilas, color=color), 3, fila)
+    tablero.posicionar(Rey(pilas, color=color), 4, fila)
+    tablero.posicionar(Alfil(pilas, color=color), 5, fila)
+    tablero.posicionar(Caballo(pilas, color=color), 6, fila)
+    tablero.posicionar(Torre(pilas, color=color), 7, fila)
+
+def acomodarPeones(pilas, tablero, fila, color):
+    for i in range(8):
+        tablero.posicionar(Peon(pilas, color=color), i, fila)
