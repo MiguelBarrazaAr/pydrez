045db2291263fc5a86d78dd756f4e1dcd191c6be
barrazamiguel <correo@miguelbarraza.com.ar>
2017-09-13 12:53:48 -0300

    incorporado un testRun

--- testRun.py
+++ testRun.py
@@ -1,9 +1,21 @@
-# -*- encoding: utf-8 -*-
+import sys
+
 import unittest
 
-from test.peonTest import TestPeon
-from test.tableroTest import TestTablero
-from test.celdaTest import TestCelda
+def run_functional_tests(pattern=None):
+    print("Running tests...")
+    if pattern is None:
+        tests = unittest.defaultTestLoader.discover("test")
+    else:
+        pattern_with_globs = "%s" % (pattern,)
+        tests = unittest.defaultTestLoader.discover("tests", pattern=pattern_with_globs)
 
-print("Running tests...")
-unittest.main()
+    runner = unittest.TextTestRunner()
+    runner.run(tests)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        run_functional_tests()
+    else:
+        run_functional_tests(pattern=sys.argv[1])
+
