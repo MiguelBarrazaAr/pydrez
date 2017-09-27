c7f7f895a581597d75420494f386044af654ecb7
Miloro <miloromiguel@gmail.com>
2017-09-27 16:34:04 -0300

    incorporado movimiento del rey

--- actor/rey.py
+++ actor/rey.py
@@ -6,4 +6,12 @@ class Rey(Ficha):
         return "rey"
 
     def puedeMoverA(self, columna, fila):
-        return True
+        if abs(self._celda.fila - fila) == 1  and abs(self._celda.columna - columna) == 1:
+            # se mueve en diagonal:
+            return True
+        elif self._celda.columna == columna and abs(self._celda.fila - fila) == 1:
+            return True
+        if self._celda.fila == fila and abs(self._celda.columna - columna) == 1:
+            return True
+        else:
+            return False
