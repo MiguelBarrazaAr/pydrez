5f084deac8a049803b14c0a8ea87fe84ea1c8cc5
barrazamiguel <correo@miguelbarraza.com.ar>
2017-09-27 15:42:08 -0300

    incorporado el movimiento del caballo

--- actor/caballo.py
+++ actor/caballo.py
@@ -7,4 +7,9 @@ class Caballo(Ficha):
 
 
     def puedeMoverA(self, columna, fila):
-        return True
+        if abs(self._celda.columna-columna) == 2 and abs(self._celda.fila-fila) == 1:
+            return True
+        elif abs(self._celda.columna-columna) == 1 and abs(self._celda.fila-fila) == 2:
+            return True
+        else:
+            return False
