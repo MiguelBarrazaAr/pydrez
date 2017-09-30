143646f57cc28403f4f1409b81946f99d5ddaffc
barrazamiguel <correo@miguelbarraza.com.ar>
2017-09-30 09:44:34 -0300

    se incluye a los comportamientos la propiedad salteadora y verificar_celdas

--- fichas/comportamiento.py
+++ fichas/comportamiento.py
@@ -5,6 +5,7 @@ class Comportamiento(object):
     def __init__(self, color):
         self.nombre = self.__class__.__name__.lower()
         self.escala = 0.7
+        self.saltadora = False
         self.color = color
         self.ficha = None
 
@@ -32,3 +33,13 @@ class Comportamiento(object):
     @property
     def fila(self):
         return self.ficha._celda.fila
+
+    def puede_comer_en(self, celda_destino):
+        """retorna true si esta ficha puede comer en esa celda_destino.
+        este metodo se debe sobreescribir si es necesario."""
+        return False
+
+    def verificar_celdas(self, celda_destino):
+        """retorna una lista de tupla (columna, fila) de las celdas que se debe verificar para llegar a celda_destino.
+        este metodo se debe sobreescribir si no es saltadora."""
+        return []
