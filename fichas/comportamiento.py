25aec6efcabd4f722b5f3c7e918c9d57fa870a11
barrazamiguel <correo@miguelbarraza.com.ar>
2017-09-30 12:27:10 -0300

    los comportamientos tienen bandos no colores

--- fichas/comportamiento.py
+++ fichas/comportamiento.py
@@ -2,18 +2,18 @@
 
 class Comportamiento(object):
 
-    def __init__(self, color):
+    def __init__(self, bando):
         self.nombre = self.__class__.__name__.lower()
         self.escala = 0.7
         self.saltadora = False
-        self.color = color
+        self.bando = bando
         self.ficha = None
 
     def activar(self, ficha):
         self.ficha = ficha
         ficha.imagen = self.ruta_de_imagen()
         ficha.nombre = self.nombre
-        ficha.color = self.color
+        ficha.color = self.bando
         if self.escala:
             ficha.escala = self.escala
 
@@ -24,7 +24,7 @@ class Comportamiento(object):
         self.ficha = None
 
     def ruta_de_imagen(self):
-        return "imagenes/fichas/"+self.color+"/"+self.nombre+".png"
+        return "imagenes/fichas/"+self.bando+"/"+self.nombre+".png"
 
     @property
     def columna(self):
@@ -34,10 +34,16 @@ class Comportamiento(object):
     def fila(self):
         return self.ficha._celda.fila
 
+    def puedeMoverA(self, columna, fila):
+        """Verifica si la pieza puede llegar a la columna y fila indicada.
+        este metodo se debe sobreescribir."""
+        return False
+
     def puedeComerEn(self, celda):
         """retorna true si esta ficha puede comer en esa celda_destino.
+        una celda no puede comer a otra del mismo bando.
         este metodo se debe sobreescribir si es necesario."""
-        return True
+        return self.puedeMoverA(columna=celda.columna, fila=celda.fila)
 
     def verificar_celdas(self, celda):
         """retorna una lista de tupla (columna, fila) de las celdas que se debe verificar para llegar a celda_destino.
--- fichas/peon.py
+++ fichas/peon.py
@@ -6,7 +6,7 @@ class Peon(Comportamiento):
     def puedeMoverA(self, columna, fila):
         """los peones podrán caminar 2 pasos si es su primer movimiento, luego solo podrá moverse de a 1 paso siempre hacia adelante.
         come 1 paso en  diagonal."""
-        if self.color == "blanco":
+        if self.bando == "blanco":
             return self._blanco_puedeMoverA(columna, fila)
         else:
             return self._negro_puedeMoverA(columna, fila)
@@ -29,7 +29,7 @@ class Peon(Comportamiento):
 
     def puedeComerEn(self, celda):
         """los peones solo pueden comer un paso en diagonal hacia adelante"""
-        if self.color == "blanco":
+        if self.bando == "blanco":
             return self.fila+1 == celda.fila and abs(celda.columna-self.columna) == 1
         else:
             return self.fila-1 == celda.fila and abs(celda.columna-self.columna) == 1
