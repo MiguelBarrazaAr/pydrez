68672e2aae0ec87279f112ccfd31f200e34c2eac
barrazamiguel <correo@miguelbarraza.com.ar>
2017-09-30 15:40:50 -0300

    incluida como pantalla el MenuPrincipal

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import pilasengine
 from accessible_output import speech
 
+from escenas.menu_principal import MenuPrincipal
 from escenas.pantallaJuego import PantallaJuego
 
 # iniciamos:
 pilas = pilasengine.iniciar(titulo='pydrez 0.1 - alpha', capturar_errores=False, habilitar_mensajes_log=False)
 s = speech.Speaker()
 
-try:
-    pilas.forzar_habilitacion_de_audio()
-except AttributeError:
-    print("Omitiendo forzar la inicializacion, version anterior a 1.4.8")
-
-
 def decir(texto, interrumpir=True, visual=False):
-  s.output(texto, interrupt=interrumpir)
-def mover(x, y, actor):
-  actor.x=actor.x+x
-  actor.y=actor.y+y
+    s.output(texto, interrupt=interrumpir)
 
+# vinculamos las pantallas:
+pilas.escenas.vincular(MenuPrincipal)
 pilas.escenas.vincular(PantallaJuego)
+
+#pilas.escenas.MenuPrincipal(pilas=pilas, tts=decir)
 pilas.escenas.PantallaJuego(pilas=pilas, tts=decir)
+
 pilas.ejecutar()
