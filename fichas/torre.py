9fc4bdcafed4eb4ef78dbb423cdbfa752929fcd9
barrazamiguel <correo@miguelbarraza.com.ar>
2017-10-01 12:11:19 -0300

    la torre no puede mover si hay fichas en su camino

--- fichas/torre.py
+++ fichas/torre.py
@@ -5,6 +5,17 @@ class Torre(Comportamiento):
 
     def puedeMoverA(self, columna, fila):
         if self.fila == fila or self.columna == columna:
-            return True
+            return self.validar_celdas_intermedias(columna, fila)
         else:
             return False
+
+    def validar_celdas_intermedias(self, columna, fila):
+        lista = []
+        if self.fila == fila:
+            for x in range(min(self.columna, columna)+1, max(self.columna, columna)):
+                lista.append((x, fila))
+        else:
+            for x in range(min(self.fila, fila)+1, max(self.fila, fila)):
+                lista.append((columna, x))
+
+        return self.validar_celdas(lista)
