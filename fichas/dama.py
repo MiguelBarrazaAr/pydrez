94e400e35faa3b9cc628ed1e8d003201c5a50d3f
barrazamiguel <correo@miguelbarraza.com.ar>
2017-10-01 22:48:30 -0300

    la dama no puede saltar fichas

--- fichas/dama.py
+++ fichas/dama.py
@@ -4,11 +4,16 @@ from .comportamiento import Comportamiento
 class Dama(Comportamiento):
 
     def puedeMoverA(self, columna, fila):
-        if self.fila == fila or self.columna == columna:
+        if self.fila == fila:
             # se mueve en vertical:
-            return True
+            return self.validar_celdas(map((lambda x: (x, fila)), range(min(self.columna, columna)+1, max(self.columna, columna))))
+        elif self.columna == columna:
+            # se mueve en orizontal:
+            return self.validar_celdas(map((lambda x: (columna, x)), range(min(self.fila, fila)+1, max(self.fila, fila))))
         elif abs(self.fila - fila) == abs(self.columna - columna):
             # se mueve en diagonal:
-            return True
+            lista1 = range(min(self.columna, columna)+1, max(self.columna, columna))
+            lista2 = range(min(self.fila, fila)+1, max(self.fila, fila))
+            return self.validar_celdas(zip(lista1, lista2))
         else:
             return False
