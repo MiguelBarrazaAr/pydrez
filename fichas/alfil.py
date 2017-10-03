8f50f4f6b76116c67f710b14e00118fa1cfdbe49
barrazamiguel <correo@miguelbarraza.com.ar>
2017-10-03 00:14:16 -0300

    corregido los movimientos de alfil y dama

--- fichas/alfil.py
+++ fichas/alfil.py
@@ -5,8 +5,14 @@ class Alfil(Comportamiento):
 
     def puedeMoverA(self, columna, fila):
         if abs(self.fila - fila) == abs(self.columna - columna):
+            # mueve en diagonal:
             lista1 = range(min(self.columna, columna)+1, max(self.columna, columna))
             lista2 = range(min(self.fila, fila)+1, max(self.fila, fila))
+            if self.columna > columna:
+                lista1.reverse()
+            if self.fila > fila:
+                lista2.reverse()
+
             return self.validar_celdas(zip(lista1, lista2))
         else:
             return False
--- fichas/dama.py
+++ fichas/dama.py
@@ -14,6 +14,11 @@ class Dama(Comportamiento):
             # se mueve en diagonal:
             lista1 = range(min(self.columna, columna)+1, max(self.columna, columna))
             lista2 = range(min(self.fila, fila)+1, max(self.fila, fila))
+            if self.columna > columna:
+                lista1.reverse()
+            if self.fila > fila:
+                lista2.reverse()
+
             return self.validar_celdas(zip(lista1, lista2))
         else:
             return False
