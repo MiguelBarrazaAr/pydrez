32815f257e7a181c04962c85799e4b9d2ad5a8c5
barrazamiguel <correo@miguelbarraza.com.ar>
2017-10-10 22:07:25 -0300

    agregado la ficha enano

---
+++ fichas/enano.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+from .comportamiento import Comportamiento
+
+class Enano(Comportamiento):
+
+    def puedeMoverA(self, columna, fila):
+        """puede mover un paso hacia cualquier lado."""
+        if abs(self.fila - fila) == 1  and abs(self.columna - columna) == 1:
+            # se mueve en diagonal:
+            return True
+        elif self.columna == columna and abs(self.fila - fila) == 1:
+            return True
+        if self.fila == fila and abs(self.columna - columna) == 1:
+            return True
+        else:
+            return False
+
+    def puedeComerEn(self, celda):
+        """Solo come en diagonal"""
+        return abs(self.fila - celda.fila) == 1  and abs(self.columna - celda.columna) == 1
