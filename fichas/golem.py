75e4c1cd5566d70936bd6a736a4a4e2a80d84a7d
barrazamiguel <correo@miguelbarraza.com.ar>
2017-10-10 22:01:26 -0300

    incorporado el golem

---
+++ fichas/golem.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+from .comportamiento import Comportamiento
+
+class Golem(Comportamiento):
+
+    def puedeMoverA(self, columna, fila):
+        if self.fila == fila and self.columna+1 == columna:
+            return True
+        if self.fila+1 == fila and self.columna == columna:
+            return True
+        else:
+            return False
+
+    def puedeComerEn(self, celda):
+        return self.fila-1 == celda.fila and abs(celda.columna-self.columna) == 1
