952a5a5c0cab2f2d761ccb09b64fa65cc9a7ce49
barrazamiguel <correo@miguelbarraza.com.ar>
2017-11-09 21:58:46 -0300

    agregada pantalla de conexion

---
+++ escenas/conectarseAlServidor.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+import pilasengine
+
+class ConectarseAlServidor(pilasengine.escenas.Escena):
+
+    def iniciar(self, pilas):
+        self.texto = pilas.actores.Texto("Escriba La IP del servidor que se quiere conectar")
+        self.entrada = pilas.interfaz.IngresoDeTexto(self.pilas)
--- escenas/desafioTernimado.py
+++
@@ -1,9 +0,0 @@
-import pilasengine
-
-
-class PantallaJuegoTerminado(pilasengine.escenas.Escena):
-    def iniciar(self, pilas, tiempo, nombreDesafio):
-        self.nombreDesafio = int(nombreDesafio)
-        self.texto_codigo = self.pilas.actores.Texto("Juego Terminado", magnitud=40)
-        self.texto_codigo2 = self.pilas.actores.Texto("tiempo: " + tiempo.texto, magnitud=40)
-        self.texto_codigo.y = 100
