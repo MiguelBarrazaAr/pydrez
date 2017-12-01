c0196efe328f54b6fb079c2030b3417b8e72b086
barrazamiguel <correo@miguelbarraza.com.ar>
2017-12-01 12:16:12 -0300

    reparado algunos errores de la regla salvaje

--- reglas/__init__.py
+++ reglas/__init__.py
@@ -3,6 +3,7 @@ from .ajedrez_tradicional import ReglasAjedrezTradicional
 from .ajedrez_atomico import ReglasAjedrezAtomico
 from .ajedrez_epico import ReglasAjedrezEpico
 from .ajedrez_minado import ReglasAjedrezMinado
+from .ajedrez_salvaje import AjedrezSalvaje
 
 from .puzzleAjedrez import PuzzleAjedrez
 
@@ -10,6 +11,7 @@ definiciones = {
     "atomico":ReglasAjedrezAtomico,
     "epico":ReglasAjedrezEpico,
     "minado":ReglasAjedrezMinado,
+    "salvaje":AjedrezSalvaje,
     "tradicional":ReglasAjedrezTradicional,
     "puzzle":PuzzleAjedrez,
 }
--- reglas/ajedrez_salvaje.py
+++ reglas/ajedrez_salvaje.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
 from .ajedrez_tradicional import ReglasAjedrezTradicional
 from sonido import Sonido
-from organizadores.ajedrez_Salvaje  import AjedrezSalvaje
+from organizadores.ajedrez_salvaje  import AjedrezSalvaje
 
-class ReglasAjedrezSalvaje(ReglasAjedrezTradicional):
+class AjedrezSalvaje(ReglasAjedrezTradicional):
 
     def __init__(self, *args, **kwargs):
         ReglasAjedrezTradicional.__init__(self, *args, **kwargs)
