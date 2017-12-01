f309f8de75deeaaae50594f0abc7b0dcb6a41b3d
Miloro <miloromiguel@gmail.com>
2017-12-01 11:53:05 -0300

    se implemento la ficha leon

---
+++ fichas/leon.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+from .comportamiento import Comportamiento
+
+class Leon(Comportamiento):
+
+    def puedeMoverA(self, columna, fila):
+        if abs(self.fila - fila) == 1  and abs(self.columna - columna) == 1:
+            # se mueve en diagonal:
+            return True
+        elif self.columna == columna and abs(self.fila - fila) == 1:
+            return True
+        if self.fila == fila and abs(self.columna - columna) == 1:
+            return True
+        else:
+            return False
