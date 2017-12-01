44f378a9d990705d688fa1314d4a7316b52da432
barrazamiguel <correo@miguelbarraza.com.ar>
2017-12-01 14:59:08 -0300

    incorporado el ajedrez en un tablero 8 x 12

--- reglas/__init__.py
+++ reglas/__init__.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from .ajedrez_tradicional import ReglasAjedrezTradicional
+from .ajedrez_8x12 import ReglasAjedrez8x12
 from .ajedrez_atomico import ReglasAjedrezAtomico
 from .ajedrez_epico import ReglasAjedrezEpico
 from .ajedrez_minado import ReglasAjedrezMinado
@@ -8,6 +9,7 @@ from .ajedrez_salvaje import ReglasAjedrezSalvaje
 from .puzzleAjedrez import PuzzleAjedrez
 
 definiciones = {
+    "ajedrez8x12":ReglasAjedrez8x12,
     "atomico":ReglasAjedrezAtomico,
     "epico":ReglasAjedrezEpico,
     "minado":ReglasAjedrezMinado,
---
+++ reglas/ajedrez_8x12.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+from .ajedrez_tradicional import ReglasAjedrezTradicional
+from sonido import Sonido
+
+class ReglasAjedrez8x12(ReglasAjedrezTradicional):
+
+    def __init__(self, *args, **kwargs):
+        ReglasAjedrezTradicional.__init__(self, *args, **kwargs)
+        self.dimensionTablero = (8, 12)
