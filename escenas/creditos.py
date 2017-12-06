a8b84d118711923f3b1c604dae923079a1ac5fd6
Miloro <miloromiguel@gmail.com>
2017-12-05 22:42:31 -0300

    se agrego una persona mas a los creditos

--- escenas/creditos.py
+++ escenas/creditos.py
@@ -8,7 +8,7 @@ class Creditos(pilasengine.escenas.Escena):
     def iniciar(self, pilas):
         marronOscuro = pilas.colores.Color(77, 38, 22)
         self.fondo = pilas.fondos.FondoMozaico("imagenes/fondo/fondoJuego.jpg")
-        self.textoCreditos = self.pilas.actores.Texto("DESARROLLADO POR:\n     Barraza Miguel\n     Miloro Miguel\nMUSICA:\n     Crispy Aguirre \nIMAGENES:\n(http://game-icons.net)\n     Skoll\n     Lorc\n     Delapouite", magnitud=15,fuente= "datos/tipografia/anirb___.ttf" )
+        self.textoCreditos = self.pilas.actores.Texto("DESARROLLADO POR:\n     Barraza Miguel\n     Miloro Miguel\nMUSICA:\n     Crispy Aguirre \nIMAGENES:\n(http://game-icons.net)\n     Skoll\n     Lorc\n     Delapouite\nhttps://opengameart.org\n     J-Robot ", magnitud=15,fuente= "datos/tipografia/anirb___.ttf" )
         self.textoCreditos.y = 0
         self.textoCreditos.color = marronOscuro
         self.pilas.eventos.pulsa_tecla_escape.conectar(self.activar_menu_principal)
